64120b8debb32051412c6ce35a9d3f890241884e
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-24 23:31:14 +0530

    UC1 Single Player Starting Point

---
+++ SinglePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Ladder
+{
+    class SinglePlayer
+    {
+        public int StartPoint = 0;
+        public void StartPlaying()
+        {
+            Console.WriteLine("Game Start Now");
+            Console.WriteLine("Starting Point is " + StartPoint);
+           
+        }
+
+
+    }
+}
