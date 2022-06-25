8e742aed9423830b7d8470a8421f1a8d3517d272
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-25 11:45:31 +0530

    UC1 Updated

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Snake_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int position = 0;
+            Console.WriteLine("Position is " + position);
+        }
+    }
+}
