21a4001a5c03432233d1eb0739d9ae9c5d03ce95
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-25 11:56:42 +0530

    UC2-GetRandomNumber

--- Program.cs
+++ Program.cs
@@ -14,6 +14,10 @@ namespace Snake_Ladder
             Console.WriteLine("Snake and Ladder game played with single player at start position 0");
             int position = 0;
             Console.WriteLine("Position is " + position);
+
+            Random random = new Random();
+            int number = random.Next(0, 7);
+            Console.WriteLine(number);
         }
     }
 }
